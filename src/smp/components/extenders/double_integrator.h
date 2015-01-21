85778ba1127c8b4c450777bd565e720dd9b71cb0
Scott Livingston <slivingston@cds.caltech.edu>
2015-01-21 06:49:23 -0500

    TRIV: Delete trailing whitespace

--- src/smp/components/extenders/double_integrator.h
+++ src/smp/components/extenders/double_integrator.h
@@ -1,7 +1,7 @@
 /*! \file components/extenders/double_integrator.h
   \brief The double integrator system components. State, input, and extender definitions.
-  
-  This file implements the state, input, and extender classes for a 
+
+  This file implements the state, input, and extender classes for a
   d-dimensional double integrator system, where d is a template parameter
   when appropriate. Currently, the implementation supports only d = 2.
   The author is working on the case when d > 2.
@@ -25,12 +25,12 @@ namespace smp {
 
     //! Implementation of the state data structure for the double integrator dynamics
     /*!
-      This class implements the state data structure for the double integrator dynamics. 
+      This class implements the state data structure for the double integrator dynamics.
       The number of state variables is twice number of dimensions, since for each dimension
       both position and velocity has to be stored. The positions are stored in the usual order
       first, and then the all velocities are stored in their usual order, in the array.
     */
-    template< int NUM_DIMENSIONS > 
+    template< int NUM_DIMENSIONS >
     class state_double_integrator : public state_array_double<NUM_DIMENSIONS*2> {
 
     };
@@ -39,14 +39,14 @@ namespace smp {
 
     //! Implementation of the input data structure for the double integrator dynamics
     /*!
-      This class implements the input data structure for the double integrator dynamics. 
+      This class implements the input data structure for the double integrator dynamics.
       The number of input variables is one plus the dimensions. The extra input variable,
-      placed in the beginning of the array, is used to store the time it takes to 
-      execute the trajectory segment. 
+      placed in the beginning of the array, is used to store the time it takes to
+      execute the trajectory segment.
     */
     template< int NUM_DIMENSIONS >
     class input_double_integrator : public input_array_double<NUM_DIMENSIONS+1> {
-    
+
     };
 
 
@@ -55,10 +55,10 @@ namespace smp {
     /*!
       This class implements an extender with double integrator dynamics. It is intended
       that the number of dimensions of the state space is a template argument for the class.
-      However, this feature is not implemented yet. Currently, the double integrator 
-      system works only in two dimensions (NUM_DIMENSIONS = 2), i.e., two positions 
+      However, this feature is not implemented yet. Currently, the double integrator
+      system works only in two dimensions (NUM_DIMENSIONS = 2), i.e., two positions
       and their two velocities.
-      
+
       \ingroup extenders
     */
     template < class typeparams, int NUM_DIMENSIONS >
@@ -75,15 +75,15 @@ namespace smp {
 
         typedef trajectory< typeparams > trajectory_t;
 
-    
+
 
         // // TODO: get back to appropriate velocity constraints.
         // double velocity_constraint_min[NUM_DIMENSIONS];
         // double velocity_constraint_max[NUM_DIMENSIONS];
 
-       
-    
-        int extend_with_optimal_control (state_t *state_ini, state_t *state_fin, 
+
+
+        int extend_with_optimal_control (state_t *state_ini, state_t *state_fin,
                                          list<state_t*> *list_states_out, list<input_t*> *list_inputs_out);
 
 
@@ -92,24 +92,24 @@ namespace smp {
         extender_double_integrator ();
         ~extender_double_integrator ();
 
-    
+
         int ex_update_insert_vertex (vertex_t *vertex_in) {return 1;}
-    
+
 
         int ex_update_insert_edge (edge_t *edge_in)  {return 1;}
 
 
         int ex_update_delete_vertex (vertex_t *vertex_in) {return 1;}
-    
-    
+
+
         int ex_update_delete_edge (edge_t *edge_in) {return 1;}
 
 
-    
+
         int extend (state_t *state_from_in, state_t *state_towards_in,
                     int *exact_connection_out, trajectory_t *trajectory_out,
                     list<state_t*> *intermediate_vertices_out);
-    
+
     };
 
 
--- src/smp/components/extenders/double_integrator.hpp
+++ src/smp/components/extenders/double_integrator.hpp
@@ -9,7 +9,7 @@
 
 
 
-// TODO: Generalize to multiple dimensions, make these configurable variables (already started in the .h file) 
+// TODO: Generalize to multiple dimensions, make these configurable variables (already started in the .h file)
 #define VELOCITY_CONSTRAINT_RANGE 2.0
 #define VELOCITY_CONSTRAINT_RANGE_2 1.0
 #define VELOCITY_CONSTRAINT_MAX 1.0
@@ -32,12 +32,12 @@ using namespace std;
 
 double extend_with_time_optimal_control_one_axis (double s_ini[2], double s_fin[2], double u_max,
 						  int *direction, int *traj_saturated,
-						  double *x_intersect_beg, double *x_intersect_end, 
+						  double *x_intersect_beg, double *x_intersect_end,
 						  double *v_intersect) {
 
 
   double u_min = -u_max;
-    
+
   // Define global variables traj_1
   bool traj_1_feasible = false;
   bool traj_1_saturated = false;
@@ -45,7 +45,7 @@ double extend_with_time_optimal_control_one_axis (double s_ini[2], double s_fin[
   double x_intersect_beg_1;
   double x_intersect_end_1;
   double v_intersect_1;
-    
+
   // Define global variables for traj_2
   bool traj_2_feasible = false;
   bool traj_2_saturated = false;
@@ -53,36 +53,36 @@ double extend_with_time_optimal_control_one_axis (double s_ini[2], double s_fin[
   double x_intersect_beg_2;
   double x_intersect_end_2;
   double v_intersect_2;
-    
+
   // 1. Calculate the traj (um, 0, -um)
   double c0_1 = s_ini[0] -  (s_ini[1] * s_ini[1])/(2.0 * u_max);
-  double c1_1 = s_fin[0] -  (s_fin[1] * s_fin[1])/(2.0 * u_min);    
+  double c1_1 = s_fin[0] -  (s_fin[1] * s_fin[1])/(2.0 * u_min);
 
   if (c0_1 < c1_1) {
     double x_intersect_1 = (c0_1 + c1_1)/2.0;
     double v_intersect_pos_1 = sqrt ( (x_intersect_1 - c0_1) * 2 * u_max );
     double v_intersect_neg_1 = -v_intersect_pos_1;
-        
+
     if ( (s_ini[1] < v_intersect_pos_1) && (s_fin[1] < v_intersect_pos_1) ) {
-            
+
       traj_1_feasible = true;
       // cout << "traj 1 feasible" << endl;
 
-            
+
       if ( (s_ini[1] < v_intersect_neg_1) && (s_fin[1] < v_intersect_neg_1) ) {
 	v_intersect_1 = v_intersect_neg_1;
       }
       else {
 	v_intersect_1 = v_intersect_pos_1;
       }
-            
+
       // ===== Consistency check === TODO: Remove this later ====
       if ( (v_intersect_1 < VELOCITY_CONSTRAINT_MIN - 0.1) ) {
 	cout << "ERR: Velocity constraint is not met :" <<  v_intersect_1 << endl;
 	exit (1);
       }
       // =====
-            
+
       if ( v_intersect_1 > VELOCITY_CONSTRAINT_MAX ) {
 	traj_1_saturated = true;
 	v_intersect_1 = VELOCITY_CONSTRAINT_MAX;
@@ -93,7 +93,7 @@ double extend_with_time_optimal_control_one_axis (double s_ini[2], double s_fin[
 	x_intersect_beg_1 = x_intersect_1;
 	x_intersect_end_1 = x_intersect_1;
       }
-            
+
       double t0_1 = (v_intersect_1 - s_ini[1])/u_max;
       double t1_1 = (s_fin[1] - v_intersect_1)/u_min;
       double ti_1 = 0.0;
@@ -101,39 +101,39 @@ double extend_with_time_optimal_control_one_axis (double s_ini[2], double s_fin[
 	ti_1 = fabs (x_intersect_end_1 - x_intersect_beg_1)/VELOCITY_CONSTRAINT_MAX;
       }
       t_tot_1 = t0_1 + t1_1 + ti_1;
-      
+
       // cout << "Times control 1 : " << t0_1 << " : " << t0_1 + ti_1 << " : " << t0_1 + ti_1 + t1_1 << endl << endl;
-      
+
     }
   }
-    
+
   // 2. Calculate the traj (um, 0, -um)
   double c0_2 = s_ini[0] -  (s_ini[1] * s_ini[1])/(2.0 * u_min);
-  double c1_2 = s_fin[0] -  (s_fin[1] * s_fin[1])/(2.0 * u_max);    
+  double c1_2 = s_fin[0] -  (s_fin[1] * s_fin[1])/(2.0 * u_max);
   if (c1_2 < c0_2) {
     double x_intersect_2 = (c0_2 + c1_2)/2.0;
     double v_intersect_pos_2 = sqrt ( (x_intersect_2 - c1_2) * 2 * u_max );
     double v_intersect_neg_2 = -v_intersect_pos_2;
-        
+
     if ( (s_ini[1] > v_intersect_neg_2) && (s_fin[1] > v_intersect_neg_2) ) {
-            
+
       traj_2_feasible = true;
       // cout << "traj 2 feasible" << endl;
-            
+
       if ( (s_ini[1] > v_intersect_pos_2) && (s_fin[1] > v_intersect_pos_2) ) {
 	v_intersect_2 = v_intersect_pos_2;
       }
       else {
 	v_intersect_2 = v_intersect_neg_2;
       }
-            
+
       // ===== Consistency check === TODO: Remove this later ====
       if ( (v_intersect_2 > VELOCITY_CONSTRAINT_MAX + 0.1) ) {
 	cout << "ERR: Velocity constraint is not met :n" <<  v_intersect_2 << endl;
 	exit (1);
       }
       // =====
-            
+
       if ( v_intersect_2 < VELOCITY_CONSTRAINT_MIN ) {
 	traj_2_saturated = true;
 	v_intersect_2 = VELOCITY_CONSTRAINT_MIN;
@@ -144,7 +144,7 @@ double extend_with_time_optimal_control_one_axis (double s_ini[2], double s_fin[
 	x_intersect_beg_2 = x_intersect_2;
 	x_intersect_end_2 = x_intersect_2;
       }
-            
+
       double t0_2 = (v_intersect_2 - s_ini[1])/u_min;
       double t1_2 = (s_fin[1] - v_intersect_2)/u_max;
       double ti_2 = 0.0;
@@ -181,7 +181,7 @@ double extend_with_time_optimal_control_one_axis (double s_ini[2], double s_fin[
     }
     return t_tot_1;
   }
-  else { 
+  else {
     if (direction != NULL) {
       *direction = -1;
       *traj_saturated = traj_2_saturated?1:0;
@@ -195,37 +195,37 @@ double extend_with_time_optimal_control_one_axis (double s_ini[2], double s_fin[
     }
     return t_tot_2;
   }
-  
+
 }
 
 
 
-int extend_with_effort_optimal_control_one_axis (double s_ini[2], double s_fin[2], double u_max, 
+int extend_with_effort_optimal_control_one_axis (double s_ini[2], double s_fin[2], double u_max,
 						 double t_min, double t_goal, double t_eps,
 						 int *dir, int *traj_saturated, double *max_control,
 						 double *x_intersect_beg, double *x_intersect_end,
 						 double *v_intersect) {
 
   // Use time_optima_control function with binary search to find the control effort that achieves time t_goal -+ t_eps
-    
+
   if (t_goal < t_min) {
     cout << "ERR: t_goal < t_min \n"<< endl;
     exit (1);
   }
-    
-    
+
+
   double t_curr = t_min;
   double u_curr = u_max/2.0;
   double u_diff = u_curr/2.0; // next difference in input: u_max/4.0;
   int max_steps = 20;
   int i = 0;
 
-      
+
   while ( (fabs (t_curr - t_goal) > t_eps) && (i++ < max_steps) ) {
-    t_curr = extend_with_time_optimal_control_one_axis (s_ini, s_fin, u_curr, 
-							dir, traj_saturated, 
+    t_curr = extend_with_time_optimal_control_one_axis (s_ini, s_fin, u_curr,
+							dir, traj_saturated,
 							x_intersect_beg, x_intersect_end, v_intersect);
-	
+
     if ( t_curr < t_goal ) {   // decrease the control effort
       u_curr -= u_diff;
     }
@@ -235,9 +235,9 @@ int extend_with_effort_optimal_control_one_axis (double s_ini[2], double s_fin[2
 
     u_diff /= 2.0;
     // printf ("u_curr: %3.5lf, t_curr: %3.5lf, t_diff: %3.5lf\n", u_curr, t_curr, t_curr- t_goal);
-  }    
+  }
 
-  // printf ("time_min: %3.5lf, time_req: %3.5lf, time_diff: %3.5lf, t_eps: %3.5lf num_steps: %d\n", 
+  // printf ("time_min: %3.5lf, time_req: %3.5lf, time_diff: %3.5lf, t_eps: %3.5lf num_steps: %d\n",
   // 	  t_min, t_goal, t_curr - t_goal, t_eps, i);
 
   *max_control = u_curr;
@@ -246,8 +246,8 @@ int extend_with_effort_optimal_control_one_axis (double s_ini[2], double s_fin[2
     // printf ("Search diverges: scanning the spectrum \n");
     // for (int i = 1; i < 100; i++) {
     //     printf ("u_curr: %3.5lf, t_curr: %3.5lf\n", 1.0/100.0 * ((double)i),
-    //             optsystem_extend_with_time_optimal_control_one_axis (s_ini, s_fin, 1.0/100.0 * ((double)i), 
-    //                                                                  dir, traj_saturated, 
+    //             optsystem_extend_with_time_optimal_control_one_axis (s_ini, s_fin, 1.0/100.0 * ((double)i),
+    //                                                                  dir, traj_saturated,
     //                                                                  x_intersect_beg, x_intersect_end, v_intersect) );
     // }
     return 0;
@@ -260,9 +260,9 @@ int extend_with_effort_optimal_control_one_axis (double s_ini[2], double s_fin[2
 
 template< class typeparams, int NUM_DIMENSIONS >
 int smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
-::extend_with_optimal_control (state_t *state_ini, state_t *state_fin, 
+::extend_with_optimal_control (state_t *state_ini, state_t *state_fin,
 			       list<state_t*> *list_states_out, list<input_t*> *list_inputs_out) {
-    
+
 
   // *states_all_out = NULL;
   // *inputs_all_out = NULL;
@@ -270,11 +270,11 @@ int smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
   list_states_out->clear();
   list_inputs_out->clear();
 
-  // 1. Extend both axes 
+  // 1. Extend both axes
   double s_ini_a1[2] = {
     (*state_ini)[0],
     (*state_ini)[2]
-  }; 
+  };
   double s_fin_a1[2] = {
     (*state_fin)[0],
     (*state_fin)[2]
@@ -291,7 +291,7 @@ int smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
   double s_ini_a2[2] = {
     (*state_ini)[1],
     (*state_ini)[3]
-  }; 
+  };
   double s_fin_a2[2] = {
     (*state_fin)[1],
     (*state_fin)[3]
@@ -319,31 +319,31 @@ int smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
 
   // 2. Compute the minimum effor control for the lagging axes
   if ( time_a1 < time_a2 - 0.001) {
-        
+
     // Compute the minimum effort control for a2
-    if ( !extend_with_effort_optimal_control_one_axis (s_ini_a1, s_fin_a1, INPUT_CONSTRAINT_MAX, 
+    if ( !extend_with_effort_optimal_control_one_axis (s_ini_a1, s_fin_a1, INPUT_CONSTRAINT_MAX,
 						       time_a1, time_a2, 0.0001,
 						       &direction_a1, &traj_saturated_a1, &max_control_a1,
-						       &x_intersect_beg_a1, &x_intersect_end_a1, 
-						       &v_intersect_a1) ) 
+						       &x_intersect_beg_a1, &x_intersect_end_a1,
+						       &v_intersect_a1) )
       return 0;
 
-                
+
   }
   else if (time_a2 < time_a1 - 0.001) {
 
     // 2.a. Compute the minimum effort control for a1
-    if( !extend_with_effort_optimal_control_one_axis (s_ini_a2, s_fin_a2, INPUT_CONSTRAINT_MAX, 
+    if( !extend_with_effort_optimal_control_one_axis (s_ini_a2, s_fin_a2, INPUT_CONSTRAINT_MAX,
 						      time_a2, time_a1, 0.0001,
 						      &direction_a2, &traj_saturated_a2, &max_control_a2,
-						      &x_intersect_beg_a2, &x_intersect_end_a2, 
+						      &x_intersect_beg_a2, &x_intersect_end_a2,
 						      &v_intersect_a2) )
       return 0;
 
   }
 
 
-  // 3. Create the merged trajectories 
+  // 3. Create the merged trajectories
   // printf ("Max controls : (%3.5lf, %3.5lf)\n", max_control_a1, max_control_a2);
 
 
@@ -353,12 +353,12 @@ int smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
 
   double t_intersect_beg_a1 = fabs ( (v_intersect_a1 - s_ini_a1[1])/max_control_a1 );
   double t_intersect_end_a1;
-  if (traj_saturated_a1) 
+  if (traj_saturated_a1)
     t_intersect_end_a1 = t_intersect_beg_a1 + fabs ( (x_intersect_beg_a1 - x_intersect_end_a1)/v_intersect_a1 );
-  else 
+  else
     t_intersect_end_a1 = t_intersect_beg_a1;
   double t_end_a1 = fabs ( (s_fin_a1[1] - v_intersect_a1)/max_control_a1 ) + t_intersect_end_a1;
- 
+
 
   // printf ("times_a1 : %3.5lf, %3.5lf, %3.5lf\n", t_intersect_beg_a1, t_intersect_end_a1, t_end_a1);
 
@@ -369,9 +369,9 @@ int smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
 
   double t_intersect_beg_a2 = fabs ( (v_intersect_a2 - s_ini_a2[1])/max_control_a2 );
   double t_intersect_end_a2;
-  if (traj_saturated_a2) 
+  if (traj_saturated_a2)
     t_intersect_end_a2 = t_intersect_beg_a2 + fabs ( (x_intersect_beg_a2 - x_intersect_end_a2)/v_intersect_a2 );
-  else 
+  else
     t_intersect_end_a2 = t_intersect_beg_a2;
   double t_end_a2 = fabs ( (s_fin_a2[1] - v_intersect_a2)/max_control_a2 ) + t_intersect_end_a2;
 
@@ -385,7 +385,7 @@ int smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
 
   double times_a1[3] = {t_intersect_beg_a1, t_intersect_end_a1, t_end_a1};
   double times_a2[3] = {t_intersect_beg_a2, t_intersect_end_a2, t_end_a2};
-    
+
   double times[6];
   int stages[6][2];
   int k_a1 = 0;
@@ -421,53 +421,53 @@ int smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
     }
   }
   // printf ("\n");
-    
+
   // Create the merging states/inputs
   double t_curr = 0.0;
-    
+
   int times_counter = 0;
 
   double min_control_a1 = -max_control_a1;
   double min_control_a2 = -max_control_a2;
 
   double c0_1_a1 = s_ini_a1[0] -  (s_ini_a1[1] * s_ini_a1[1])/(2.0 * max_control_a1);
-  double c1_1_a1 = s_fin_a1[0] -  (s_fin_a1[1] * s_fin_a1[1])/(2.0 * min_control_a1);    
+  double c1_1_a1 = s_fin_a1[0] -  (s_fin_a1[1] * s_fin_a1[1])/(2.0 * min_control_a1);
   double c0_2_a1 = s_ini_a1[0] -  (s_ini_a1[1] * s_ini_a1[1])/(2.0 * min_control_a1);
-  double c1_2_a1 = s_fin_a1[0] -  (s_fin_a1[1] * s_fin_a1[1])/(2.0 * max_control_a1);    
+  double c1_2_a1 = s_fin_a1[0] -  (s_fin_a1[1] * s_fin_a1[1])/(2.0 * max_control_a1);
 
   double c0_1_a2 = s_ini_a2[0] -  (s_ini_a2[1] * s_ini_a2[1])/(2.0 * max_control_a2);
-  double c1_1_a2 = s_fin_a2[0] -  (s_fin_a2[1] * s_fin_a2[1])/(2.0 * min_control_a2);    
+  double c1_1_a2 = s_fin_a2[0] -  (s_fin_a2[1] * s_fin_a2[1])/(2.0 * min_control_a2);
   double c0_2_a2 = s_ini_a2[0] -  (s_ini_a2[1] * s_ini_a2[1])/(2.0 * min_control_a2);
-  double c1_2_a2 = s_fin_a2[0] -  (s_fin_a2[1] * s_fin_a2[1])/(2.0 * max_control_a2);    
+  double c1_2_a2 = s_fin_a2[0] -  (s_fin_a2[1] * s_fin_a2[1])/(2.0 * max_control_a2);
 
   // printf ("times: \n");
-  // for (int i = 0; i < 6; i++) 
-  //   printf (" %3.5lf - (%d,%d) - (%3.5lf,%3.5lf)\n", times[i], stages[i][0], stages[i][1], 
+  // for (int i = 0; i < 6; i++)
+  //   printf (" %3.5lf - (%d,%d) - (%3.5lf,%3.5lf)\n", times[i], stages[i][0], stages[i][1],
   // 	    times_a1[stages[i][0]-1], times_a2[stages[i][1]-1]);
   // printf ("\n");
-    
-  while (times_counter < 6) { 
+
+  while (times_counter < 6) {
 
     int increment_times_counter = 0;
-        
+
     // Determine the current time to act
     double del_t = DELTA_T;
     t_curr += DELTA_T;
 
     if (t_curr > times[times_counter]) {
       del_t -= t_curr - times[times_counter];
-      t_curr = times[times_counter]; 
+      t_curr = times[times_counter];
       increment_times_counter = 1;
     }
 
-    // Calculate the states/inputs at the current time 
+    // Calculate the states/inputs at the current time
     state_t *state_new = new state_t;
     input_t *input_new = new input_t;
 
     //      Determine the first axis at this time step
     double t_diff_curr;
     if (stages[times_counter][0] == 1) {
-      (*state_new)[2] = s_ini_a1[1] + direction_a1 * max_control_a1 * t_curr; 
+      (*state_new)[2] = s_ini_a1[1] + direction_a1 * max_control_a1 * t_curr;
       (*state_new)[0] = ((*state_new)[2] * (*state_new)[2])/(2 * direction_a1 * max_control_a1);
       if (direction_a1 == 1) {
 	(*state_new)[0] += c0_1_a1;
@@ -484,7 +484,7 @@ int smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
     }
     else {
       t_diff_curr = times_a1[2] - t_curr;
-      (*state_new)[2] = s_fin_a1[1] 
+      (*state_new)[2] = s_fin_a1[1]
 	- direction_a1 * min_control_a1 * t_diff_curr;
       (*state_new)[0] = ((*state_new)[2] * (*state_new)[2])/(2 * direction_a1 * min_control_a1);
       if (direction_a1 == 1) {
@@ -495,11 +495,11 @@ int smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
       }
 
       (*input_new)[1] = direction_a1 * min_control_a1;
-    }        
-                    
+    }
+
     //      Determine the second axis at this time step
     if (stages[times_counter][1] == 1) {
-      (*state_new)[3] = s_ini_a2[1] + direction_a2 * max_control_a2 * t_curr; 
+      (*state_new)[3] = s_ini_a2[1] + direction_a2 * max_control_a2 * t_curr;
       (*state_new)[1] = ((*state_new)[3] * (*state_new)[3])/(2 * direction_a2 * max_control_a2);
       if (direction_a2 == 1) {
 	(*state_new)[1] += c0_1_a2;
@@ -507,7 +507,7 @@ int smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
       else {
 	(*state_new)[1] += c0_2_a2;
       }
-            
+
       (*input_new)[2] = direction_a2 * max_control_a2;
     }
     else if (stages[times_counter][1] == 2) {
@@ -532,7 +532,7 @@ int smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
 
     (*input_new)[0] = del_t;
 
-    // printf ("(%d, %d, %d) - t_curr: %2.5lf - a1: (%3.5lf , %3.5lf , %3.5lf) - a2:  (%3.5lf , %3.5lf , %3.5lf)\n", 
+    // printf ("(%d, %d, %d) - t_curr: %2.5lf - a1: (%3.5lf , %3.5lf , %3.5lf) - a2:  (%3.5lf , %3.5lf , %3.5lf)\n",
     // 	    times_counter, stages[times_counter][0], stages[times_counter][1],
     // 	    t_curr, state_new->state_vars[0], state_new->state_vars[2], input_new->input_vars[0], state_new->state_vars[1], state_new->state_vars[3], input_new->x[1]);
 
@@ -543,7 +543,7 @@ int smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
 
     // Check whether we are done
     if (increment_times_counter == 1) {
-      while (times_counter < 6) 
+      while (times_counter < 6)
 	if ( fabs (times[times_counter] - times[times_counter+1]) < 0.0001 )
 	  times_counter++;
 	else
@@ -551,7 +551,7 @@ int smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
       times_counter++;
     }
   }
-    
+
   return 1;
 }
 
@@ -569,14 +569,14 @@ int smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
 template <class typeparams, int NUM_DIMENSIONS>
 smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
 ::extender_double_integrator () {
-  
+
 }
 
 
 template <class typeparams, int NUM_DIMENSIONS>
 smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
 ::~extender_double_integrator () {
-  
+
 }
 
 
@@ -585,7 +585,7 @@ int smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
 ::extend (state_t *state_from_in, state_t *state_towards_in,
 	  int *exact_connection_out, trajectory_t *trajectory_out,
 	  list<state_t*> *intermediate_vertices_out) {
-  
+
   // cout << "state_from";
   // for (int i = 0; i < 4; i++)
   //   cout << " : " << state_from_in->state_vars[i];
@@ -596,22 +596,20 @@ int smp::extender_double_integrator<typeparams,NUM_DIMENSIONS>
   //   cout << " : " << state_towards_in->state_vars[i];
   // cout << endl;
 
-  
+
   intermediate_vertices_out->clear ();
-  
+
   if (extend_with_optimal_control(state_from_in, state_towards_in,
 				  &(trajectory_out->list_states), &(trajectory_out->list_inputs)) == 0) {
-    
+
     return 0;
   }
 
 
   *exact_connection_out = 1;
-  
+
   return 1;
 }
 
 
 #endif
-
-
