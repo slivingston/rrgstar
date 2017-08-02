51dedb865e6e67246c8fd49ba4a0a7ed8b1d6019
Scott C. Livingston <slivingston@cds.caltech.edu>
2017-08-02 15:56:57 -0700

    Define M_PI and M_PI_2 for non-UNIX-like platforms

--- src/components/extenders/dubins_double_integrator.hpp
+++ src/components/extenders/dubins_double_integrator.hpp
@@ -13,6 +13,12 @@
 #include <iostream>
 #include <cstdlib>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+#ifndef M_PI_2
+#define M_PI_2 1.57079632679489661923
+#endif
 
 #ifndef DBL_MAX
 #define DBL_MAX 10000000000000000.0
